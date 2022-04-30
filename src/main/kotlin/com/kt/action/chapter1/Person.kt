155656b5f656d8a080797d3a53be8f840e2f0317
gisungchoi <chlrltjd159@naver.com>
2022-04-30 15:15:07 +0900

    feat : 맛보기

---
+++ src/main/kotlin/com/kt/action/chapter1/Person.kt
@@ -0,0 +1,16 @@
+package com.kt.action.chapter1
+
+data class Person(
+    val name: String,
+    val age: Int? = null
+)
+
+fun main(args: Array<String>) {
+    val persons = listOf(
+        Person("영희"),
+        Person("철수", age = 29)
+    )
+
+    val oldset = persons.maxByOrNull { it.age ?: 0 }
+    println("나이가 많은 사람 : $oldset")
+}
