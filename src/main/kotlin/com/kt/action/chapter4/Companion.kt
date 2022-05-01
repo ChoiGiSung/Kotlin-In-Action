a8aa1639ae958cf29c485c406a7e98307951f5d2
gisungchoi <chlrltjd159@naver.com>
2022-05-01 18:09:29 +0900

    feat : 동반 객체 응용
    - 이름을 붙일 수 있다
    - 상속과 구현을 할 수 있다
    - 파라미터로 넘길 수 있다. 부모를 넘기면 동반 객체가 넘어간다

--- src/main/kotlin/com/kt/action/chapter4/Companion.kt
+++ src/main/kotlin/com/kt/action/chapter4/Companion.kt
@@ -1,11 +1,21 @@
 package com.kt.action.chapter4
 
 class CUser private constructor(val nickname: String) {
-    companion object {
-        fun newSubUser(email: String) {
+    companion object InnerUser : User {
 
-        }
+        fun newSubUser(email: String) = CUser(email)
 
         fun newFaceBookUser(account: Int) = CUser(account.toString())
+
+        override val nickName: String
+            get() = ""
     }
+}
+
+fun main(args: Array<String>) {
+    load(CUser)
+}
+
+fun load(user: User) {
+    println(user.toString())
 }
