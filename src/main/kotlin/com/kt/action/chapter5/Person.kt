0f2ea42d5db7c482d811a9dd769e1f3b5844037c
gisungchoi <chlrltjd159@naver.com>
2022-05-02 01:59:32 +0900

    feat : 람다에서 마지막 람다는 괄호 밖으로 뺄 수 있다
    - 람다를 인자로 받는 함수일 경우 마지막 람다는 밖으로 뺄 수 있다

--- src/main/kotlin/com/kt/action/chapter5/Person.kt
+++ src/main/kotlin/com/kt/action/chapter5/Person.kt
@@ -1,5 +1,7 @@
 package com.kt.action.chapter5
 
+import com.kt.action.chapter3.joinToString
+
 data class Person(val name: String, val age: Int)
 
 fun main(args: Array<String>) {
@@ -11,4 +13,15 @@ fun main(args: Array<String>) {
 
     println(sum(1, 2))
     run { println(sum(1, 2)) }
+}
+
+fun lambdaJoinToString() {
+    val people = listOf(Person("철수", 1), Person("영희", 2))
+
+    people.joinToString(
+        " ",
+        transform = { person: Person -> person.name }
+    )
+
+    people.joinToString("") { person: Person -> person.name }
 }
