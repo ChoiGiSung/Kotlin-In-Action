652b753f356f7a942253be64794f55b185d16154
gisungchoi <chlrltjd159@naver.com>
2022-05-02 23:30:01 +0900

    feat : 시퀀스의 특징
    - 시퀀스는 하나 하나 넘기면서 넘어간다
    - 예로 1을 계산하고 find 검사하고 다음 2를 계산하고 find 검사하고 이런식
    - 시퀀스가 아닌 것은 map 검사하고 filter 검사하고 한단계 한단계씩 함
    - 이로 인해 성능이 변할 수 있음
    - 시퀀스는 자바의 스트림과 동일함 -> 안드로이드를 위해 만들어짐 -> 스트림은 병렬 스트림이 있어 더 좋음

--- src/main/kotlin/com/kt/action/chapter5/Person.kt
+++ src/main/kotlin/com/kt/action/chapter5/Person.kt
@@ -13,6 +13,8 @@ fun main(args: Array<String>) {
 
     println(sum(1, 2))
     run { println(sum(1, 2)) }
+
+    layLoading()
 }
 
 fun lambdaJoinToString() {
@@ -43,16 +45,28 @@ fun printProblemCounts(responses: Collection<String>) {
 fun flatMap() {
     class Book(val title: String, val authors: List<String>)
 
-    val books = listOf(Book("책1", listOf("저자1","저자2")))
+    val books = listOf(Book("책1", listOf("저자1", "저자2")))
 
     books.flatMap { it.authors }.toSet()
 
 }
 
-fun layLoading(){
+fun layLoading() {
     val people = listOf(Person("철수", 1), Person("영희", 2))
     people.asSequence()
         .map(Person::name)
         .filter { it.startsWith("철") }
         .toList()
+
+    val find = listOf(1, 2, 3, 4).asSequence()
+        .map { it * it }
+        .find { it > 3 }
+
+    print("find = $find")
+
+    val find1 = listOf(1, 2, 3, 4)
+        .map { it * it }
+        .find { it > 3 }
+
+    print("find = $find1")
 }
