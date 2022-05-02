67d1d13939a13d483c2879f0791515c03e64cc73
gisungchoi <chlrltjd159@naver.com>
2022-05-03 00:19:26 +0900

    feat : apply 함수
    - with과 동일하지만 수신객체를 반환한다
    - apply는 객체의 인스턴스를 만들면서 즉시 프로퍼티 중 일부를 초기화 해야하는 경우 유용함. builder와 비슷함
    - buildString 도 함수, 매개변수는 수신 객체 지정 람다

--- src/main/kotlin/com/kt/action/chapter5/Lamdb.kt
+++ src/main/kotlin/com/kt/action/chapter5/Lamdb.kt
@@ -1,9 +1,23 @@
 package com.kt.action.chapter5
 
-fun alphabet() = with(StringBuilder()) {
+fun alphabetWith() = with(StringBuilder()) {
     for (letter in 'A'..'Z') {
         append(letter)
     }
     append(" ha ha ")
     toString()
 }
+
+fun alphabetApply() = StringBuilder().apply {
+    for (letter in 'A'..'Z') {
+        append(letter)
+    }
+    append(" ha ha ")
+}.toString()
+
+fun alphabetBuildString() = buildString {
+    for (letter in 'A'..'Z') {
+        append(letter)
+    }
+    append(" ha ha ")
+}.toString()
