7063c116f13c49970c2b54af6a747fa16ffff147
gisungchoi <chlrltjd159@naver.com>
2022-05-03 13:54:57 +0900

    feat : nullable type에 대한 확장 함수
    - null이 될 수 있는 타입에 확장 함수는 ?.나 !!를 사용하지 않는다

--- src/main/kotlin/com/kt/action/chapter6/LamdbFun.kt
+++ src/main/kotlin/com/kt/action/chapter6/LamdbFun.kt
@@ -1,10 +1,15 @@
 package com.kt.action.chapter6
 
+fun main() {
+    val listOf = listOf("민수")
+    listOf[0].let { sendEmail(it) }
+}
+
 fun sendEmail(email: String) {
     println("$email")
 }
 
-fun main() {
-    val listOf = listOf("민수")
-    listOf[0].let { sendEmail(it) }
+fun isNullOrEmpty(input: String?) {
+    println(input?.toString())
+    println(input.isNullOrEmpty())
 }
