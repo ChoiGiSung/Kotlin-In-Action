5779a765a56ea3fdbb65eec3bf2268ef1f142e17
gisungchoi <chlrltjd159@naver.com>
2022-05-03 21:44:29 +0900

    feat : 위임프로퍼티 예시
    - getValue, setValue등은 이미 hashMap이 구현해 놨음
    - 구현을 key로 찾아서 이름을 불러야한다
    - 이처럼 위임 프로퍼티를 통해 프로퍼티에 대한 특정 작업을 캡슐화할 수 있다

--- src/main/kotlin/com/kt/action/chapter7/Foo.kt
+++ src/main/kotlin/com/kt/action/chapter7/Foo.kt
@@ -6,3 +6,20 @@ class Foo {
 
     var p: Type by Delegate()
 }
+
+class Person {
+    private val _attributes = hashMapOf<String, String>()
+
+    fun setAttribute(attrName: String, value: String) {
+        _attributes[attrName] = value
+    }
+
+    val name: String by _attributes
+}
+
+fun main(){
+    val person = Person()
+
+    person.setAttribute("name","영희")
+    println(person.name)
+}
