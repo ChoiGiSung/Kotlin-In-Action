6a3efafa9520eed633156c12749e2245f0010ab0
gisungchoi <chlrltjd159@naver.com>
2022-05-03 14:01:56 +0900

    feat : let도 nuable에 쓸 수 있다
    - nullable한 곳에 쓸 수 있지만 괄호안에서 그것의 type도 nuable하게 관리된다

--- src/main/kotlin/com/kt/action/chapter6/LamdbFun.kt
+++ src/main/kotlin/com/kt/action/chapter6/LamdbFun.kt
@@ -2,7 +2,7 @@ package com.kt.action.chapter6
 
 fun main() {
     val listOf = listOf("민수")
-    listOf[0].let { sendEmail(it) }
+    listOf[0]?.let { sendEmail(it) }
 }
 
 fun sendEmail(email: String) {
