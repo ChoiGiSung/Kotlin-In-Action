5c336728f4ec1a75f3cf9f8779873b4a9a08289f
gisungchoi <chlrltjd159@naver.com>
2022-05-05 17:16:18 +0900

    feat : 제네릭은 타입이 실행 시점에 지워짐
    - 지워지기에 무슨 List인지 검사는 불가능하다
    - 지워짐으로서의 이점은 저장해야하는 타입 정보의 크기가 줄기에 전반적인 메모리 사용량이 줄어든다
    
    - 리스트는 리스트인줄 알 수 있지만 map은 리스트인지 검사하려면 스타 프로젝션(*)을 사용해야한다

--- src/main/kotlin/com/kt/action/chapter9/Generic.kt
+++ src/main/kotlin/com/kt/action/chapter9/Generic.kt
@@ -1,8 +1,25 @@
 package com.kt.action.chapter9
 
+fun main(){
+    typeErasure()
+}
+
 fun <T> ensureTrailingPeriod(seq: T)
         where T : CharSequence, T : Appendable {
     if (!seq.endsWith('.')) {
         seq.append('.')
+    }
+}
+
+fun typeErasure(){
+    val listOf = listOf(1, 2)
+    val mapOf = mapOf("1" to "one")
+
+    if (listOf is List){ //List<Int> -> x
+        println("리스트?")
+    }
+
+    if (mapOf is List<*>){
+        println("리스트?")
     }
 }
