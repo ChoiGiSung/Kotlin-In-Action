e0a9c81b2af4608aa3ba2a3dfbaacfaae7d4fe1e
gisungchoi <chlrltjd159@naver.com>
2022-05-05 18:33:45 +0900

    feat : out 키워드 해결
    - out을 사용하면 공변적인 클래스로 만들면 된다
    - 하위 클래스도 공변적으로 바꿀 수 있다
    - 하위 클래스도 공변을 선언해야지만 사용가능하다
    
    - 타입 파라미터를 공변적으로 만들면 항상 out 위치에 존재해야한다
    - 생산하는 위치(반환)에 있어야 한다. in(파라미터)의 위치에는 있을 수 없다

--- src/main/kotlin/com/kt/action/chapter9/Covariant.kt
+++ src/main/kotlin/com/kt/action/chapter9/Covariant.kt
@@ -6,7 +6,7 @@ open class Animal {
     }
 }
 
-class Herd<T : Animal> {
+class Herd<out T : Animal> {
     val size: Int get() = 2
 
     fun feedAll(animals: Herd<Animal>){
@@ -18,9 +18,13 @@ class Herd<T : Animal> {
 
     fun takeCareOfCats(cats: Herd<Cat>){
         for ( i in 0 until cats.size ){
-//            feedAll(cats) 에러 발생
+            feedAll(cats)
         }
     }
+//
+//    fun inOut(s:T){
+//        컴파일 에러
+//    }
 
 }
 
