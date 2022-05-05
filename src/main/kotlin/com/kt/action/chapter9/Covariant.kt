0b217a72835348730ceb1932bd99ea9a8f536f2f
gisungchoi <chlrltjd159@naver.com>
2022-05-05 18:28:36 +0900

    feat : out 키워드
    - 클래스의 타입을 공변적으로 만들면 함수 정의에 사용한 타입과 인자의 타입이 정확히 일치하지 않더라도 그 클래스이 인스턴슬르 함수 인자나 반환값으로 사용할 수 있다
    - cat은 animal의 하위 클래스이긴 하지만 변성을 선언해주지 않았기에 하위 클래스가 아니다
    - 타입 캐스팅을 하면 풀 수 있지만 번거롭고 올바른 방법이 아니다

--- src/main/kotlin/com/kt/action/chapter9/Covariant.kt
+++ src/main/kotlin/com/kt/action/chapter9/Covariant.kt
@@ -1,5 +1,29 @@
 package com.kt.action.chapter9
 
-interface Producer<out T> {
-    fun produce(): T
+open class Animal {
+    fun feed() {
+
+    }
+}
+
+class Herd<T : Animal> {
+    val size: Int get() = 2
+
+    fun feedAll(animals: Herd<Animal>){
+        for ( i in 0 until animals.size){
+
+        }
+    }
+
+
+    fun takeCareOfCats(cats: Herd<Cat>){
+        for ( i in 0 until cats.size ){
+//            feedAll(cats) 에러 발생
+        }
+    }
+
+}
+
+class Cat : Animal(){
+
 }
