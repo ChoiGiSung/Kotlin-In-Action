fcf69fb01f4839c9cf5d7982b60c12f4992bcb74
gisungchoi <chlrltjd159@naver.com>
2022-05-05 18:02:25 +0900

    feat : 서비스로더를 런타임 시점에 사용해 보기
    - 해당하는 타입을 런타임 시점에 파악하여 인스턴스를 구할 수 있다

--- src/main/kotlin/com/kt/action/chapter9/Inline.kt
+++ src/main/kotlin/com/kt/action/chapter9/Inline.kt
@@ -1,13 +1,21 @@
 package com.kt.action.chapter9
 
+import java.util.*
+
 fun main() {
     isA<String>(listOf("A"))
 }
 
 inline fun <reified T> isA(value: Any) = value is T
 
-fun filterReified(){
+fun filterReified() {
     val listOf = listOf(1, "2", 3, "4")
     val filterIsInstance = listOf.filterIsInstance<String>()
     //filter -> 2,3
 }
+
+fun serviceLoader() {
+    loadService<String>()
+}
+
+inline fun <reified T> loadService() = ServiceLoader.load(T::class.java)
